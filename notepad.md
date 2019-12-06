d3f372a818453e1ae40c4cf16d1a4c397178d03a
Ali Darab <alidarab@5-IMAC272017D25XXF0G5J1GQ.local>
2019-12-06 14:10:26 -0500

    Added Functionality to take user input and output the actor ID

--- notepad.md
+++ notepad.md
@@ -13,3 +13,23 @@ getActorDetails.then(response => {
   console.log(data)
   console.log(`${data.results[0].known_for[0].backdrop_path}`)
 }).catch(handeError) */
+
+
+const pokePromise1 = $.ajax({
+  url : 'https://pokeapi.co/api/v2/pokemon/1/',
+  dataType : 'json',
+  method: 'GET'
+});
+
+const pokePromise2 = $.ajax({
+  url : 'https://pokeapi.co/api/v2/pokemon/1/',
+  dataType : 'json',
+  method: 'GET'
+});
+
+$.when(pokePromise1, pokePromise2).done(function(pokemon1, pokemon2){
+  console.log(pokemon1);
+  console.log(pokemon2);
+
+  // Do whatever else you need to do with your new pokemon data
+})
--- script.js
+++ script.js
@@ -1,22 +1,48 @@
+// Get User Input to pass a search Query to the API
+// The query then searches for the actor Details and gets the actor's ID
+// The Returned ID then needs to be passed onto another API call that takes in the ID and searches for 
+
+
 const app = {};
 
 // API Key
 app.apiKey = `d9aa9ee0be2d538327e8e9474bd49cbf`;
-app.baseUrl = `https://api.themoviedb.org/3/search/person`;
-app.imagesUrl = `http://image.tmdb.org/t/p`;
+app.searchUrl = `https://api.themoviedb.org/3/search/person`;
+app.creditsURL = `https://api.themoviedb.org/3/person/${showActorID()}/combined_credits?`
+app.imagesUrl = `http://image.tmdb.org/t/p/w300_and_h450_bestv2`;
 
-const getActorDetails = $.ajax({
-  type: 'GET',
-  url: `${app.baseUrl}`,
-  dataType: 'json',
-  data: {
-    api_key: `${app.apiKey}`,
-    query: `Tom Hanks`,
-    language: `en-US`,
-    include_adult: `false`,
-    page: 1,
-  },
-}).done(function(response) {
-  const getActorID = `${response.results[0].id}`;
-  console.log(getActorID);
+// This function takes the User Query and gets the object, only the ID is selected.
+app.getActorDetails = (userQuery) => {
+  $.ajax({
+    type: 'GET',
+    url: `${app.searchUrl}`,
+    dataType: 'json',
+    data: {
+      api_key: `${app.apiKey}`,
+      query: userQuery,
+      language: `en-US`,
+      include_adult: `false`,
+      page: 1,
+    }
+    }).then(function(response) {
+      app.showActorID(response.results[0].id)
+    });
+}
+
+app.showActorID = function(data){
+  // console.log(data);
+  return data
+}
+
+
+app.init = function() {
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    const userSelection = $('input').val();
+    app.getActorDetails(userSelection);
+  });
+}
+
+$(function () {
+  app.init();
 });
