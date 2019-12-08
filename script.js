02880448fe4e9f71cd05080ba99ad1b0994394e8
Ali Darab <alidarab@live.com>
2019-12-07 23:02:06 -0500

    Successfuly setup promise

--- script.js
+++ script.js
@@ -1,61 +1,60 @@
-// Get User Input to pass a search Query to the API
-// The query then searches for the actor Details and gets the actor's ID
-// The Returned ID then needs to be passed onto another API call that takes in the ID and searches for
-// Next Step: Make two getActorDetails Calls, and get IDs and pass them onto the an API Call that looks for two actors
-// Have Brad Pitt and Edward Norton ever been in a movie together?
-// URL: /discover/movie?with_people=287,819&sort_by=vote_average.desc
-
 const app = {};
 
 // API Key
 app.apiKey = `d9aa9ee0be2d538327e8e9474bd49cbf`;
 app.searchUrl = `https://api.themoviedb.org/3/search/person`;
+app.discoverURL = `https://api.themoviedb.org/3/discover/movie`;
 app.imagesUrl = `http://image.tmdb.org/t/p/w300_and_h450_bestv2`;
 
-// This function takes the User Query and gets the object, only the ID is selected.
-app.getActorDetails = userQuery => {
-  $.ajax({
-    type: 'GET',
-    url: `${app.searchUrl}`,
-    dataType: 'json',
-    data: {
-      api_key: `${app.apiKey}`,
-      query: userQuery,
-      language: `en-US`,
-      include_adult: `false`,
-      page: 1,
-    },
-  }).then(function(response) {
-    app.getCredits(response.results[0].id);
-  });
-};
+app.firstActorQuery = '';
+app.secondActorQuery = '';
 
-app.creditsURL = `https://api.themoviedb.org/3/person`;
-
-app.getCredits = actorID => {
-  $.ajax({
-    url: `${app.creditsURL}/${actorID}/combined_credits`,
-    method: 'GET',
-    dataType: 'json',
-    data: {
-      api_key: `${app.apiKey}`,
-      language: `en-US`,
-    },
-  }).then(responseFromID => {
-    console.log(responseFromID.cast);
-    console.log(`Tom Hanks starred in ${responseFromID.cast[0].title}`);
-    console.log(`Tom Hanks starred in ${responseFromID.cast[1].title}`);
-  });
-};
-
-app.init = function() {
-  $('form').on('submit', function(e) {
-    e.preventDefault();
-    const userSelection = $('input').val();
-    app.getActorDetails(userSelection);
-  });
-};
-
-$(function() {
-  app.init();
+app.getFirstActorID = $.ajax({
+  type: 'GET',
+  url: `${app.searchUrl}`,
+  dataType: 'json',
+  data: {
+    api_key: `${app.apiKey}`,
+    query: 'Tom Hanks',
+    language: `en-US`,
+    include_adult: `false`,
+    page: 1,
+  },
 });
+
+app.getSecondActorID = $.ajax({
+  type: 'GET',
+  url: `${app.searchUrl}`,
+  dataType: 'json',
+  data: {
+    api_key: `${app.apiKey}`,
+    query: 'Brad Pitt',
+    language: `en-US`,
+    include_adult: `false`,
+    page: 1,
+  },
+});
+
+$.when(app.getFirstActorID, app.getSecondActorID)
+  .done(function(actor1, actor2) {
+    // console.log(actor1[0].results[0].id);
+    app.firstActorID = actor1[0].results[0].id;
+    // console.log(actor2[0].results[0].id);
+    app.secondActorID = actor2[0].results[0].id;
+  })
+  .then(function() {
+    console.log(app.firstActorID, app.secondActorID);
+
+    app.getMovies = $.ajax({
+      type: 'json',
+      url: `${app.discoverURL}`,
+      dataType: 'json',
+      data: {
+        api_key: `${app.apiKey}`,
+        sort_by: `vote_average.desc`,
+        with_people: `${app.firstActorID},${app.secondActorID}`,
+        language: `en-US`,
+        page: 1,
+      },
+    });
+  });
--- script2.js
+++ script2.js
@@ -1,3 +1,10 @@
+// Get User Input to pass a search Query to the API
+// The query then searches for the actor Details and gets the actor's ID
+// The Returned ID then needs to be passed onto another API call that takes in the ID and searches for
+// Next Step: Make two getActorDetails Calls, and get IDs and pass them onto the an API Call that looks for two actors
+// Have Brad Pitt and Edward Norton ever been in a movie together?
+// URL: /discover/movie?with_people=287,819&sort_by=vote_average.desc
+
 const app = {};
 
 // API Key
@@ -5,44 +12,47 @@ app.apiKey = `d9aa9ee0be2d538327e8e9474bd49cbf`;
 app.searchUrl = `https://api.themoviedb.org/3/search/person`;
 app.imagesUrl = `http://image.tmdb.org/t/p/w300_and_h450_bestv2`;
 
-app.firstActorQuery = ''
-app.secondActorQuery = ''
-
-
 // This function takes the User Query and gets the object, only the ID is selected.
-app.getFirstActorID = $.ajax({ // !Added Return just before $ajax
+app.getActorDetails = userQuery => {
+  $.ajax({
     type: 'GET',
     url: `${app.searchUrl}`,
     dataType: 'json',
     data: {
       api_key: `${app.apiKey}`,
-      query: 'Tom Hanks',
+      query: userQuery,
       language: `en-US`,
       include_adult: `false`,
       page: 1,
     },
+  }).then(function(response) {
+    app.getCredits(response.results[0].id);
   });
+};
 
+app.creditsURL = `https://api.themoviedb.org/3/person`;
 
-
-app.getSecondActorID = $.ajax({
-    type: 'GET',
-    url: `${app.searchUrl}`,
+app.getCredits = actorID => {
+  $.ajax({
+    url: `${app.creditsURL}/${actorID}/combined_credits`,
+    method: 'GET',
     dataType: 'json',
     data: {
       api_key: `${app.apiKey}`,
-      query: 'Brad Pitt',
       language: `en-US`,
-      include_adult: `false`,
-      page: 1,
     },
+  }).then(responseFromID => {
+    console.log(responseFromID.cast);
+    console.log(`Tom Hanks starred in ${responseFromID.cast[0].title}`);
+    console.log(`Tom Hanks starred in ${responseFromID.cast[1].title}`);
   });
-
+};
 
 app.init = function() {
-  $.when(app.getFirstActorID, app.getSecondActorID).done(function(actor1, actor2) {
-    console.log(actor1);
-    console.log(actor2);
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    const userSelection = $('input').val();
+    app.getActorDetails(userSelection);
   });
 };
 
