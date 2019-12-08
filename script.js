0db801170b3bbb33327d0531ebf04f768dbab7a6
Ali Darab <alidarab@live.com>
2019-12-08 15:41:12 -0500

    Working Output to HTML before allowing user Input

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ const app = {};
 // API Key
 app.apiKey = `d9aa9ee0be2d538327e8e9474bd49cbf`;
 app.searchUrl = `https://api.themoviedb.org/3/search/person`;
-app.discoverURL = `https://api.themoviedb.org/3/discover/movie`;
+app.discoverUrl = `https://api.themoviedb.org/3/discover/movie`;
 app.imagesUrl = `http://image.tmdb.org/t/p/w300_and_h450_bestv2`;
 
-app.firstActorQuery = '';
-app.secondActorQuery = '';
+app.firstActorQuery = `Seth Rogen`;
+app.secondActorQuery = `James Franco`;
 
+// make API calls to get actor details
 app.getFirstActorDetails = $.ajax({
   type: 'GET',
   url: `${app.searchUrl}`,
   dataType: 'json',
   data: {
     api_key: `${app.apiKey}`,
-    query: 'Seth Rogen',
+    query: `${app.firstActorQuery}`,
     language: `en-US`,
-    include_adult: `false`,
+    include_adult: `true`,
   },
 });
 
@@ -27,45 +28,57 @@ app.getSecondActorDetails = $.ajax({
   dataType: 'json',
   data: {
     api_key: `${app.apiKey}`,
-    query: 'James Franco',
+    query: `${app.secondActorQuery}`,
     language: `en-US`,
-    include_adult: `false`,
+    include_adult: `true`,
   },
 });
 
+// promise return for actor details
 $.when(app.getFirstActorDetails, app.getSecondActorDetails)
   .done(function(actor1, actor2) {
-    app.firstActor = {
-      id: actor1[0].results[0].id,
-      name: actor1[0].results[0].name,
-      photo: `${app.imagesUrl}${actor1[0].results[0].profile_path}`,
-    };
-    console.log(app.firstActor.id, app.firstActor.name, app.firstActor.photo);
-    // console.log(actor1[0].results[0].name);
-    // console.log(`${app.imagesUrl}${actor1[0].results[0].profile_path}`);
-    // app.firstActorID = actor1[0].results[0].id;
-    // console.log(actor2[0].results[0].id);
-    // app.secondActorID = actor2[0].results[0].id;
+    // store the actors' name, id, and photo. To reduce repetition, the objects are storred in arrays
+    app.actorDetails = [
+      {
+        id: actor1[0].results[0].id,
+        name: actor1[0].results[0].name,
+        photo: `${app.imagesUrl}${actor1[0].results[0].profile_path}`,
+      },
+      {
+        id: actor2[0].results[0].id,
+        name: actor2[0].results[0].name,
+        photo: `${app.imagesUrl}${actor2[0].results[0].profile_path}`,
+      },
+    ];
   })
-  .then(function() {
+  .then(function getAllMovies() {
     app.getMovies = $.ajax({
       type: 'GET',
-      url: `${app.discoverURL}`,
+      url: `${app.discoverUrl}`,
       dataType: 'json',
       data: {
         api_key: `${app.apiKey}`,
         sort_by: `vote_average.desc`,
-        with_people: `${app.firstActorID},${app.secondActorID}`,
+        with_people: `${app.actorDetails[0].id},${app.actorDetails[1].id}`,
         language: `en-US`,
         page: 1,
+        include_adult: true,
       },
       success(response) {
-        for (let i = 0; i < 10; i++) {
-          console.log(response.results[i].title);
+        // Display information of Top 10 movies both actors have worked together in.
+        for (let i = 0; i < 12; i++) {
+          $('#movie-info-container').append(
+            `<div class="col-4 movie-info animated fadeIn">
+            <img src='${app.imagesUrl}${response.results[i].poster_path}' alt='${response.results[i].title}'>
+            <h4>${response.results[i].title}</h4>
+            </div>`
+          );
         }
       },
     });
   })
   .catch(function(error) {
-    console.log(`That's an error.`);
+    $('#movie-info-container').append(
+      `<h1>Sorry, there are no movies in which they've worked</h1>`
+    );
   });
